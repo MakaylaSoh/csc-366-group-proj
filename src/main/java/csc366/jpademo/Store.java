3255af4915b5fbfced3a3584a22560d7c14bfa29
MakaylaSoh <mcsoh@calpoly.edu>
2024-03-03 01:11:31 -0800

    updated store class

--- src/main/java/csc366/jpademo/Store.java
+++ src/main/java/csc366/jpademo/Store.java
@@ -23,8 +23,93 @@ public class Store {
     @GeneratedValue(strategy=GenerationType.IDENTITY) // generates id
     private Long id;
 
-    private String country;
-    private String city;
+    @Column(name="country")
+    private String country;  
+
+    @Column(name="state")
     private String state;
+
+    @Nullable
+    @Column(name="city")
+    private String city;  
+
+    @Column(name="street")
+    private String street;  
+    
+    @Column(name="zipcode")
+    private int zipcode;
+    
+    public Store() { }
+    
+    public Store(String country, String state, String city, String street, int zipcode) {
+        this.country = country;
+        this.state = state;
+        this.cty = city;
+        this.street = street;
+        this.zipCode = zipCode;
+    }
+    
+    public Long getId() {
+	    return id;
+    }
+
+    public void setId(Long id) {
+	    this.id = id;
+    }
+
+    public String getCountry() {
+	    return state;
+    }
+
+    public void setCountry(String state) {
+	    this.state = state;
+    }
+
+    public String getState() {
+	    return state;
+    }
+
+    public void setState(String state) {
+	    this.state = state;
+    }
+
+    public String getCity() {
+	    return city;
+    }
+
+    public void setCity(String city) {
+	    this.city = city;
+    }
+
+    public String getStreet() {
+	    return street;
+    }
+
+    public void setStreet(String street) {
+	    this.street = street;
+    }
+
+    public int getZipcode() {
+	    return zipCode;
+    }
+
+    public void setZipcode(int zipcode) {
+	    this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString()
+    {
+	    StringJoiner sj = new StringJoiner("," , Address.class.getSimpleName() + "[" , "]");
+	    sj.add(id.toString()).add(street).add(city).add(state).add(zipCode);
+	    return sj.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+	    if (this == o) return true;
+	    if (!(o instanceof Address)) return false;
+	    return id != null && id.equals(((Address) o).getId());
+    }
     
 }
